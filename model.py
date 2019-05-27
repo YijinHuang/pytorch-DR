0ccf3291636240e73a4d045b6a77cbf416781317
Eugene Huang <eugenehuang1998@gmail.com>
2019-05-28 00:36:00 +0800

    Use type A net as o_ONet, reimplement unitied bias and parameters initialization

--- model.py
+++ model.py
@@ -9,30 +9,31 @@ class o_ONet(nn.Module):
 
         # 1-11 layers
         small_conv = nn.Sequential(
-            self.basic_conv2d(3, 32, kernel_size=4, stride=2, padding=1),
-            self.basic_conv2d(32, 32, kernel_size=4, stride=1, padding=2),
+            self.basic_conv2d(3, 32, 224, 224, kernel_size=5, stride=2, padding=2),
+            self.basic_conv2d(32, 32, 224, 224, kernel_size=3, stride=1, padding=1),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=0),
-            self.basic_conv2d(32, 64, kernel_size=4, stride=2, padding=1),
-            self.basic_conv2d(64, 64, kernel_size=4, stride=1, padding=2),
-            self.basic_conv2d(64, 64, kernel_size=4, stride=1, padding=1),
+            self.basic_conv2d(32, 64, 56, 56, kernel_size=5, stride=2, padding=2),
+            self.basic_conv2d(64, 64, 56, 56, kernel_size=3, stride=1, padding=1),
+            self.basic_conv2d(64, 64, 56, 56, kernel_size=3, stride=1, padding=1),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=0),
-            self.basic_conv2d(64, 128, kernel_size=4, stride=1, padding=2),
-            self.basic_conv2d(128, 128, kernel_size=4, stride=1, padding=1),
-            self.basic_conv2d(128, 128, kernel_size=4, stride=1, padding=2),
+            self.basic_conv2d(64, 128, 27, 27, kernel_size=3, stride=1, padding=1),
+            self.basic_conv2d(128, 128, 27, 27, kernel_size=3, stride=1, padding=1),
+            self.basic_conv2d(128, 128, 27, 27, kernel_size=3, stride=1, padding=1),
         )
 
         # 12-15 layers
         medium_conv = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=2, padding=0),
-            self.basic_conv2d(128, 256, kernel_size=4, stride=1, padding=2),
-            self.basic_conv2d(256, 256, kernel_size=4, stride=1, padding=1),
-            self.basic_conv2d(256, 256, kernel_size=4, stride=1, padding=2),
+            self.basic_conv2d(128, 256, 13, 13, kernel_size=3, stride=1, padding=1),
+            self.basic_conv2d(256, 256, 13, 13, kernel_size=3, stride=1, padding=1),
+            self.basic_conv2d(256, 256, 13, 13, kernel_size=3, stride=1, padding=1),
         )
 
         # 16-17 layers (without 18 layer for net B)
         large_conv = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=2, padding=0),
-            self.basic_conv2d(256, 512, kernel_size=4, stride=1, padding=1),
+            self.basic_conv2d(256, 512, 6, 6, kernel_size=3, stride=1, padding=1),
+            self.basic_conv2d(512, 512, 6, 6, kernel_size=3, stride=1, padding=1),
         )
 
         # name layers
@@ -60,15 +61,13 @@ class o_ONet(nn.Module):
 
         # initial parameters
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
+            if isinstance(m, Conv2dUntiedBias) or isinstance(m, nn.Linear):
+                nn.init.orthogonal_(m.weight, 1)
+                nn.init.constant_(m.bias, 0.05)
 
-    def basic_conv2d(self, in_channels, out_channels, kernel_size, stride, padding):
+    def basic_conv2d(self, in_channels, out_channels, height, width, kernel_size, stride, padding):
         return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+            Conv2dUntiedBias(in_channels, out_channels, height, width, kernel_size, stride, padding),
             nn.LeakyReLU(negative_slope=0.01)
         )
 
@@ -89,6 +88,9 @@ class o_ONet(nn.Module):
 
         # exclude
         for name in list(pretrained_dict.keys()):
+            # using untied biases will make it unable to reload.
+            if 'bias' in name:
+                pretrained_dict.pop(name)
             for e in exclude:
                 if e in name:
                     pretrained_dict.pop(name)
@@ -111,3 +113,21 @@ class RMSPool(nn.Module):
         x = F.avg_pool2d(x, kernel_size=3, stride=2)
         x = torch.sqrt(x)
         return x
+
+
+class Conv2dUntiedBias(nn.Module):
+    def __init__(self, in_channels, out_channels, height, width, kernel_size, stride=1, padding=0):
+        super(Conv2dUntiedBias, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = (kernel_size, kernel_size)
+        self.stride = (stride, stride)
+        self.padding = (padding, padding)
+        self.weight = nn.Parameter(torch.Tensor(out_channels, in_channels, kernel_size, kernel_size))
+        self.bias = nn.Parameter(torch.Tensor(out_channels, height, width))
+
+    def forward(self, x):
+        output = F.conv2d(x, self.weight, None, self.stride, self.padding)
+        output += self.bias.unsqueeze(0).repeat(x.size(0), 1, 1, 1)
+        return output
