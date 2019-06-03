516a85ab2822d4e7c1f9d66b664e0308f0826dd3
Eugene Huang <eugenehuang1998@gmail.com>
2019-06-03 18:20:28 +0800

    modify config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 SMALL_NET_CONFIG = {
     'NET_SIZE': 'small',
-    'DATA_PATH': '../../dataset/train_data_full_112',
-    'SAVE_PATH': '../../results/o_O_results/o_O_small.pt',
+    'DATA_PATH': '../../dataset/train_data_full_128',
+    'SAVE_PATH': '../../results/o_O_result/o_O_small.pt',
     'PRETRAINED_PATH': None,
     'LEARNING_RATE': 3e-3,
     'INPUT_SIZE': 112,
@@ -19,9 +19,9 @@ SMALL_NET_CONFIG = {
 
 MEDIUM_NET_CONFIG = {
     'NET_SIZE': 'medium',
-    'DATA_PATH': '../../dataset/train_data_full_224',
-    'SAVE_PATH': '../../results/o_O_results/o_O_medium.pt',
-    'PRETRAINED_PATH': '../../results/o_O_results/o_O_small.pt',
+    'DATA_PATH': '../../dataset/train_data_full_256',
+    'SAVE_PATH': '../../results/o_O_result/o_O_medium.pt',
+    'PRETRAINED_PATH': '../../results/o_O_result/o_O_small.pt',
     'LEARNING_RATE': 3e-3,
     'INPUT_SIZE': 224,
     'FEATURE_DIM': 1024,
@@ -38,9 +38,9 @@ MEDIUM_NET_CONFIG = {
 
 LARGE_NET_CONFIG = {
     'NET_SIZE': 'large',
-    'DATA_PATH': '../../dataset/train_data_full_448',
-    'SAVE_PATH': '../../results/o_O_results/o_O_large.pt',
-    'PRETRAINED_PATH': '../../results/o_O_results/o_O_medium.pt',
+    'DATA_PATH': '../../dataset/train_data_full_512',
+    'SAVE_PATH': '../../results/o_O_result/o_O_large.pt',
+    'PRETRAINED_PATH': '../../results/o_O_result/o_O_medium.pt',
     'LEARNING_RATE': 3e-3,
     'INPUT_SIZE': 448,
     'FEATURE_DIM': 2048,
