8480ecca253968a83cbeb28e499d9181612fa86c
Eugene Huang <eugenehuang1998@gmail.com>
2019-07-17 13:14:25 +0800

    add warmup and focal loss

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 SMALL_NET_CONFIG = {
     'NET_SIZE': 'small',
     'DATA_PATH': '../../dataset/two_cates_train_data_full_128',
-    'SAVE_PATH': '../../result/o_O_result/two_cates_o_O_small.pt',
+    'SAVE_PATH': '../../result/o_O_result/two_cates_o_O_fl_warmup_small.pt',
     'PRETRAINED_PATH': None,
     'LEARNING_RATE': 3e-3,
     'INPUT_SIZE': 112,
     'FEATURE_DIM': 512,
     'BATCH_SIZE': 128,
-    'EPOCHS': 200,
+    'EPOCHS': 210,
     'DATA_AUGMENTATION': {
         'scale': (1 / 1.15, 1.15),
         'stretch_ratio': (0.7561, 1.3225),  # (1/(1.15*1.15) and 1.15*1.15)
@@ -20,13 +20,13 @@ SMALL_NET_CONFIG = {
 MEDIUM_NET_CONFIG = {
     'NET_SIZE': 'medium',
     'DATA_PATH': '../../dataset/two_cates_train_data_full_256',
-    'SAVE_PATH': '../../results/o_O_result/two_cates_o_O_medium.pt',
-    'PRETRAINED_PATH': '../../result/o_O_result/two_cates_o_O_small.pt',
+    'SAVE_PATH': '../../result/o_O_result/two_cates_o_O_fl_warmup_medium.pt',
+    'PRETRAINED_PATH': '../../result/o_O_result/two_cates_o_O_fl_warmup_small.pt',
     'LEARNING_RATE': 3e-3,
     'INPUT_SIZE': 224,
     'FEATURE_DIM': 1024,
     'BATCH_SIZE': 128,
-    'EPOCHS': 200,
+    'EPOCHS': 210,
     'DATA_AUGMENTATION': {
         'scale': (1 / 1.15, 1.15),
         'stretch_ratio': (0.7561, 1.3225),  # (1/(1.15*1.15) and 1.15*1.15)
@@ -39,13 +39,13 @@ MEDIUM_NET_CONFIG = {
 LARGE_NET_CONFIG = {
     'NET_SIZE': 'large',
     'DATA_PATH': '../../dataset/two_cates_train_data_full_512',
-    'SAVE_PATH': '../../results/o_O_result/two_cates_o_O_large.pt',
-    'PRETRAINED_PATH': '../../result/o_O_result/two_cates_o_O_medium.pt',
+    'SAVE_PATH': '../../result/o_O_result/two_cates_o_O_fl_warmup_large.pt',
+    'PRETRAINED_PATH': '../../result/o_O_result/two_cates_o_O_fl_warmup_medium.pt',
     'LEARNING_RATE': 3e-3,
     'INPUT_SIZE': 448,
     'FEATURE_DIM': 2048,
     'BATCH_SIZE': 48,
-    'EPOCHS': 250,
+    'EPOCHS': 270,
     'DATA_AUGMENTATION': {
         'scale': (1 / 1.15, 1.15),
         'stretch_ratio': (0.7561, 1.3225),  # (1/(1.15*1.15) and 1.15*1.15)
--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 from tqdm import tqdm
+import torch.nn.functional as F
+from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
 from data_utils import ScheduledWeightedSampler
@@ -25,12 +27,16 @@ def train(net, net_size, input_size, feature_dim, train_dataset, val_dataset,
         print_msg('Loaded weights from {}: '.format(pretrained_model), sorted(pretrained_dict.keys()))
 
     # define loss and optimizier
-    crossEntropyLoss = torch.nn.CrossEntropyLoss()
+    focal_loss = FocalLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, nesterov=True, weight_decay=0.0005)
 
-    # learning rate decay
-    milestones = [150, 220]
+    # learning rate warmup and decay
+    milestones = [160, 230]
+    warmup_epoch = 10
+    warmup_batch = (len(train_loader) // batch_size) * warmup_epoch
+
     lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=0.1)
+    warmup_scheduler = WarmupLRScheduler(optimizer, warmup_batch, learning_rate)
 
     # train
     max_kappa = 0
@@ -43,19 +49,26 @@ def train(net, net_size, input_size, feature_dim, train_dataset, val_dataset,
         # learning rate update
         lr_scheduler.step()
         if epoch in milestones:
-            print_msg('Learning rate decayed to {}'.format(lr_scheduler.get_lr()[0]))
+            curr_lr = optimizer.param_groups[0]['lr']
+            print_msg('Learning rate decayed to {}'.format(curr_lr))
+        if epoch > 1 and epoch <= warmup_epoch:
+            curr_lr = optimizer.param_groups[0]['lr']
+            print_msg('Learning rate warmup to {}'.format(curr_lr))
 
         epoch_loss = 0
         correct = 0
         total = 0
         progress = tqdm(enumerate(train_loader))
         for step, train_data in progress:
+            if epoch <= warmup_epoch:
+                warmup_scheduler.step()
+ 
             X, y = train_data
-            X, y = X.cuda(), y.long().cuda()
+            X, y = X.cuda(), y.cuda()
 
             # forward
             y_pred = model(X)
-            loss = crossEntropyLoss(y_pred, y)
+            loss = focal_loss(y_pred, y)
 
             # backward
             optimizer.zero_grad()
@@ -132,3 +145,54 @@ def print_msg(msg, appendixs=[]):
     for appendix in appendixs:
         print(appendix)
     print('=' * max_len)
+
+# reference: https://github.com/clcarwin/focal_loss_pytorch/blob/master/focalloss.py
+class FocalLoss(torch.nn.Module):
+    def __init__(self, gamma=0, alpha=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha, (float, int)):
+            self.alpha = torch.Tensor([alpha, 1 - alpha])
+        if isinstance(alpha, list):
+            self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim() > 2:
+            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1, 2)    # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1, input.size(2))   # N,H*W,C => N*H*W,C
+        target = target.view(-1, 1)
+
+        logpt = F.log_softmax(input)
+        logpt = logpt.gather(1, target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        if self.alpha is not None:
+            if self.alpha.type() != input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0, target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1 - pt)**self.gamma * logpt
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
+
+
+class WarmupLRScheduler:
+    def __init__(self, optimizer, warmup_batch, initial_lr):
+        self.step_num = 1
+        self.optimizer = optimizer
+        self.warmup_batch = warmup_batch
+        self.initial_lr = initial_lr
+
+    def step(self):
+        if self.step_num <= self.warmup_batch:
+            self.step_num += 1
+            curr_lr = (self.step_num / self.warmup_batch) * self.initial_lr
+            for param_group in self.optimizer.param_groups:
+                param_group['lr'] = curr_lr
