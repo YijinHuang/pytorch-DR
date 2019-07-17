6e614d41a526fbfcb6ee7daf2ef385b20253df40
Eugene Huang <eugenehuang1998@gmail.com>
2019-07-17 21:30:15 +0800

    1.fix wrong translation_ratio in config; 2.update README

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - This work is based on team o_O solution for the Kaggle Diabetic Retinopathy Detection Challenge.
 - [Solution summary](https://www.kaggle.com/c/diabetic-retinopathy-detection/discussion/15617#latest-373487)
 
+### Branches
+
+- Branch "master" tries to apply new techniques to improve team o_O solution.
+- Branch "reimplement" is the reimplementation of team o_O solution.
+- Branch "two_cates" is used to do binary classification (Normal/DR).
+
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ SMALL_NET_CONFIG = {
     'SAVE_PATH': '../../result/o_O_result/Res_o_O_deeper_small.pt',
     'PRETRAINED_PATH': None,
     'LEARNING_RATE': 3e-3,
-    'INPUT_SIZE': 112,
+    'INPUT_SIZE': (112, 112)
     'FEATURE_DIM': 1152,
     'BATCH_SIZE': 128,
     'EPOCHS': 210,
@@ -23,7 +23,7 @@ MEDIUM_NET_CONFIG = {
     'SAVE_PATH': '../../result/o_O_result/Res_o_O_deeper_medium.pt',
     'PRETRAINED_PATH': '../../result/o_O_result/Res_o_O_deeper_small.pt',
     'LEARNING_RATE': 3e-3,
-    'INPUT_SIZE': 224,
+    'INPUT_SIZE': (224, 224)
     'FEATURE_DIM': 2304,
     'BATCH_SIZE': 128,
     'EPOCHS': 210,
@@ -31,7 +31,7 @@ MEDIUM_NET_CONFIG = {
         'scale': (1 / 1.15, 1.15),
         'stretch_ratio': (0.7561, 1.3225),  # (1/(1.15*1.15) and 1.15*1.15)
         'ratation': (-180, 180),
-        'translation_ratio': (40 / 112, 40 / 112),  # 40 pixel in the report
+        'translation_ratio': (40 / 224, 40 / 224),  # 40 pixel in the report
         'sigma': 0.5
     }
 }
@@ -42,7 +42,7 @@ LARGE_NET_CONFIG = {
     'SAVE_PATH': '../../result/o_O_result/Res_o_O_deeper_large.pt',
     'PRETRAINED_PATH': '../../result/o_O_result/Res_o_O_deeper_medium.pt',
     'LEARNING_RATE': 3e-3,
-    'INPUT_SIZE': 448,
+    'INPUT_SIZE': (448, 448)
     'FEATURE_DIM': 4608,
     'BATCH_SIZE': 48,
     'EPOCHS': 270,
@@ -50,7 +50,7 @@ LARGE_NET_CONFIG = {
         'scale': (1 / 1.15, 1.15),
         'stretch_ratio': (0.7561, 1.3225),  # (1/(1.15*1.15) and 1.15*1.15)
         'ratation': (-180, 180),
-        'translation_ratio': (40 / 112, 40 / 112),  # 40 pixel in the report
+        'translation_ratio': (40 / 448, 40 / 448),  # 40 pixel in the report
         'sigma': 0.5
     }
 }
